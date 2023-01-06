a3c12f41a7a5a722abf062f394f8c7c66e1716c7
jarbasai <jarbasai@mailfence.com>
2023-01-06 14:40:08 +0000

    scrap automation

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from os.path import join, dirname
+import random
 
 from ovos_plugin_common_play.ocp import MediaType, PlaybackType
 from ovos_utils.log import LOG
@@ -24,12 +25,14 @@ class DocumentaryCentralSkill(OVOSCommonPlaybackSkill):
                                                           "opening theme"])
 
     def initialize(self):
-        url = "https://www.youtube.com/channel/UCp4wHZCrDhITkiUILOd4gRw"
         bootstrap = "https://github.com/JarbasSkills/skill-documentary-central/raw/dev/bootstrap.json"
         self.archive.bootstrap_from_url(bootstrap)
-        self.archive.monitor(url)
-        self.archive.setDaemon(True)
-        self.archive.start()
+        self.schedule_event(self._sync_db, random.randint(3600, 24 * 3600))
+
+    def _sync_db(self):
+        url = "https://www.youtube.com/channel/UCp4wHZCrDhITkiUILOd4gRw"
+        self.archive.parse_videos(url)
+        self.schedule_event(self._sync_db, random.randint(3600, 24*3600))
 
     # matching
     def match_skill(self, phrase, media_type):
